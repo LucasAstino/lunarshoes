28341cf51016771bb9b627794e48b6bf17839d75
LucasAstino <116663021+LucasAstino@users.noreply.github.com>
2023-02-20 18:06:23 -0300

    finalized
    finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
